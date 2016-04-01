d9203f70cceed43fafde6e8955d9ce2fbc5e95db
HuguesM <hugues.martinelli@gmail.com>
2016-04-01 16:17:33 +0200

    Add thread&comment

--- CatExercise/CodeFirstCat/User.cs
+++ CatExercise/CodeFirstCat/User.cs
@@ -5,22 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CodeFirstCat {
-    class User {
-    public int UserID { get; set; }
-    public String Login { get; set; }
-    public String Password { get; set; }
-    public String Pseudo { get; set; }
-    public Boolean Banish { get; set; }
-    public int Seclevel { get; set; }
-    public DateTime Creationdate { get; set; }
-    public User(String login, String password) {
-        this.Login = login;
-        this.Password = password;
-    }
-    public User() { 
-    
-    }
-    public static int ADMINISTRATEUR=100;
-    public static int UTILISATEUR=0;
+    public class User {
+        public int UserID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String Pseudo { get; set; }
+        public Boolean Banish { get; set; }
+        public int Seclevel { get; set; }
+        public DateTime Creationdate { get; set; }
+        public ICollection<CatThread> CatThreads { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+        public User(String login, String password) {
+            this.Login = login;
+            this.Password = password;
+        }
+        public User() {
+
+        }
+        public static int ADMINISTRATEUR = 100;
+        public static int UTILISATEUR = 0;
     }
 }
