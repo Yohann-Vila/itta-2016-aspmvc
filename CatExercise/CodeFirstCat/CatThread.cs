a8bc4dbe8c310d58e906599c90757355472f7112
Yohann-Vila <yohann.vila@signatys.com>
2016-04-01 16:11:41 +0200

    Comments class

--- CatExercise/CodeFirstCat/CatThread.cs
+++ CatExercise/CodeFirstCat/CatThread.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeFirstCat
 {
-    public class Class1
+    class CatThread
     {
     }
 }
---
+++ CatExercise/CodeFirstCat/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstCat
+{
+    public class Comment
+    {
+        public int CatThreadId { get; set; }
+        public String Login { get; set; }
+        public String Titre { get; set; }
+        public String UriPhoto { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime? CreationDate { get; set; }
+
+        public Comment()
+        {
+            Deleted = false;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
